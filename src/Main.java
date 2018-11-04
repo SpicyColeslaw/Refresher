ab27d7de6185bcd211caf1723cb8556e1a4930f9
Josh Cole <colejc@mail.missouri.edu>
2018-11-04 14:26:28 -0600

    Testing the projects connection to github. making simple changes that will most likely be changed again.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(args[0]);
+        System.out.println("I would like to test changing something in git.");
     }
 }
