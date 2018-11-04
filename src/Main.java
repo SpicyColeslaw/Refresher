28a4f57c6aaecce36266f62d6ab9e16ae88fb5d6
SpicyColeslaw <colejc@mail.missouri.edu>
2018-11-04 14:36:59 -0600

    Changing Main.java from browser
    changing Main.java from browser to test out pull function in IntelliJ.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("I would like to test changing something in git.");
+        System.out.println("Uh oh.");
     }
 }
